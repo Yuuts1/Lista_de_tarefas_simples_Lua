1d1373f8d4e45fb63f1f89c2ff961047b1e8ccb9
Italo Dariff <105507380+Yuuts1@users.noreply.github.com>
2024-05-22 08:10:25 -0300

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,17 +1,17 @@
+tarefas = {}
 local io = require("io")
 
 while true do
-    print("Lista de Tarefas: \n Escolha as opcoes: \n")
-    print("1: Escrever tarefa \n")
-    print("2: Mostra tarefas")
+    print("Lista de Tarefas")
+    print("1: Adicionar \n 2: Rever \n 3: Sair")
     local escolha = tonumber( io.read())
 
     if escolha == 1 then
-        print("Digite sua anotação: \n")
-        anatocao = io.read()
-    
+        print("Digite o que deseja adicionar")
+        tarefas = io.read()
     elseif escolha == 2 then
-        print(anatocao)  
-        break  
+        print(tarefas)
+    else
+        break
     end
 end
